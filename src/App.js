3033bdb88552d8491d6e7f9c37bfb71ff64e29af
Saurabh <saurabhgola2006@gmail.com>
2024-11-11 17:37:50 +0530

    test latest new test envfile

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>NEW-WEBSITE</h1>
       <a href="https://reactjs.org">Learn React</a>  {/* Add the link if necessary */}
+      <br/>
+      <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>
   </>
   );

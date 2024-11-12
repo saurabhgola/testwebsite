a4a3a9c1ca3d1e38431c8b81990aafdcc2d1e948
Saurabh <saurabhgola2006@gmail.com>
2024-11-12 18:54:55 +0530

    new branch Newdeveloped-new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>NEW-WEBSITE</h1>
       <h2>Welcome</h2>
       <h2>New test branch</h2>
-      <h2>this new branch</h2>
+      <h2>this Newdeveloped-new</h2>
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>

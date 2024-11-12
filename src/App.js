57a8ff1c95402e0d266eb76e769b7e070b133f93
Saurabh <saurabhgola2006@gmail.com>
2024-11-12 12:37:13 +0530

    new changes appfile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   <>
     <div className="App">
       <h1>NEW-WEBSITE</h1>
-      <h1>TEST EXAMPLE</h1>
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>

bf274f9043f22e924e17a21464caca4281d0755d
Saurabh <saurabhgola2006@gmail.com>
2024-11-12 11:37:30 +0530

    new changes yaml

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   <>
     <div className="App">
       <h1>NEW-WEBSITE</h1>
-      <h2>Hello</h2>
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>

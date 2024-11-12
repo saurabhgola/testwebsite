cdd1a282e091daab69923e41cea98c5589f8fc6b
Saurabh <saurabhgola2006@gmail.com>
2024-11-12 23:41:36 +0530

    new branch netlify west

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <h1>NEW-WEBSITE</h1>
       <h2>Welcome</h2>
       <h2>New test branch</h2>
+      <h3>Netlify Test west</h3>
       <h3>Netlify Test</h3>
+
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>

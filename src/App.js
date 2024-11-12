70c2a51bf21b8f66d7176a95d5232ea95ced9d46
saurabhgola <152872918+saurabhgola@users.noreply.github.com>
2024-11-13 00:25:43 +0530

    Netlify web test (#15)
    * test new
    
    * new test
    
    * test new branch test
    
    * new read cd branch
    
    * test breanch develope
    
    * test branch2
    
    * test ready branch
    
    * test 5
    
    * test this main
    
    * new changes test
    
    * test  app json file
    
    * new changes djson
    
    * newd test this
    
    * newd test
    
    * new yaml file create
    
    * new app file1
    
    * new app file2
    
    * new branch
    
    * new branch Newdeveloped-new
    
    * new branch git
    
    * new branch git
    
    * new branch netlify
    
    * new branch netlify west
    
    * new branch netlify west yaml
    
    * new branch netlify west yaml
    
    * new netlify test
    
    * new netlify test-test
    
    * new netlify next

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ function App() {
       <h1>NEW-WEBSITE</h1>
       <h2>Welcome</h2>
       <h2>New test branch</h2>
-=======
-      <h2>this Newdeveloped-new</h2>
-      <br/>
-
-    <h3>Test branch</h3>
-    
+     <h3>Netlify Test test</h3>
  <br/>
     <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>

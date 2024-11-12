9930649aa97e6f5ea79e7b122514b17d90370d59
saurabhgola <152872918+saurabhgola@users.noreply.github.com>
2024-11-12 18:41:08 +0530

    Newdeveloped (#6)
    * test new
    
    * new test
    
    * test new branch test
    
    * new read cd branch
    
    * test breanch develope
    
    * test branch2
    
    * test ready branch
    
    * test 5
    
    * test this main
    
    * new changes test
    
    * test  app json file
    
    * new changes djson
    
    * newd test this
    
    * newd test
    
    * new yaml file create
    
    * new app file1
    
    * new app file2

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
       <h1>NEW-WEBSITE</h1>
       <h2>Welcome</h2>
       <h2>New test branch</h2>
+      <br/>
 
     <h3>Test branch</h3>
     
  <br/>
-      <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
+    <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>
   </>
   );

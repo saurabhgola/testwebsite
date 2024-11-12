1ff1788aa6537f252757084255d57f1900550666
Saurabh <saurabhgola2006@gmail.com>
2024-11-13 00:03:12 +0530

    new netlify test

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
       <h2>Welcome</h2>
       <h2>New test branch</h2>
       <h3>Netlify Test west</h3>
-      <h3>Netlify Test</h3>
-
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
     </div>

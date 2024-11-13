15dc5be1b1c9def45ca0221a66171ff0c7bd403f
saurabhgola <152872918+saurabhgola@users.noreply.github.com>
2024-11-13 17:25:57 +0530

    Test create netlify (#27)
    * test new
    
    * new test
    
    * test new branch test
    
    * new read cd branch
    
    * test breanch develope
    
    * test branch2
    
    * test ready branch
    
    * test 5
    
    * test this main
    
    * new changes test
    
    * test  app json file
    
    * new changes djson
    
    * newd test this
    
    * newd test
    
    * new yaml file create
    
    * new app file1
    
    * new app file2
    
    * new branch
    
    * new branch Newdeveloped-new
    
    * new branch git
    
    * new branch git
    
    * new branch netlify
    
    * new branch netlify west
    
    * new branch netlify west yaml
    
    * new branch netlify west yaml
    
    * new netlify test
    
    * new netlify test-test
    
    * new netlify next
    
    * new netlify developed
    
    * new test-netlify
    
    * new test-site
    
    * new new-website
    
    * new website
    
    * test is new
    
    * test that
    
    * test that test
    
    * test that test build
    
    * test create netlify
    
    * test create add
    
    * test creat
    
    * Fix Netlify configuration for base directory
    
    * Fix Netlify configuration for base

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   <>
     <div className="App">
       <h1>NEW-WEBSITE</h1>
-      <h2>Welcome</h2>
       <h2>New test branch</h2>
      <h3>Netlify Production</h3>
      <h2>New site is create</h2>
+     <h3>netlify test new build</h3>
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>

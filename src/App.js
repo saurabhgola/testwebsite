97adeddac1761aa622bfa039a5d82f9940dd2e58
Saurabh <saurabhgola2006@gmail.com>
2024-11-13 11:24:06 +0530

    new new-website

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <h1>NEW-WEBSITE</h1>
       <h2>Welcome</h2>
       <h2>New test branch</h2>
-     <h3>Netlify Test test</h3>
+     <h3>Netlify web</h3>
+     <h3>Netlify Production</h3>
       <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>

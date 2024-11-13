0410254f72cc800194ec130b508f2a0602891bd0
saurabhgola <152872918+saurabhgola@users.noreply.github.com>
2024-11-13 22:35:18 +0530

    Branch website (#32)
    * new changes yaml file
    
    * new changes yaml file
    
    * test  app file
    
    * Fix Netli new-branch
    
    * Fix Netli new-json
    
    * Fix leatest

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ function App() {
   <>
     <div className="App">
       <h1>NEW-WEBSITE</h1>
-      <h2>New test branch</h2>
-     <h3>Netlify Production</h3>
-     <h2>New site is create</h2>
-     <h3>netlify test new build</h3>
-      <br/>
+      <h2>tets</h2>
+      <h1>WEBSITE NEW</h1>
+           <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>
     </div>

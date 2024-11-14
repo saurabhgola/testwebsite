8f61b461ac9f954645eb25e8fc1a97d0cf976692
Saurabh <saurabhgola2006@gmail.com>
2024-11-14 17:07:04 +0530

    fix text-new

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ function App() {
   return (
   <>
     <div className="App">
-      <h1>NEW-WEBSITE</h1>
      <h3>Netlify Production</h3>
      <h1>Hello welcome WEBSITE</h1>
      <h3>netlify test new build</h3>
-     <h1>new changes silver-two-test</h1>
+     <h1>new changes </h1>
       <br/>
       <h1>WEBSITE NEW</h1>
-      <h2>test this part</h2>
            <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>

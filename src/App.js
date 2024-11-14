9ce05469a206978c79ed0b8fd1165b4d35fa31c2
Saurabh <saurabhgola2006@gmail.com>
2024-11-14 12:28:11 +0530

    Fix leatest this netlify-js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <h2>tets</h2>
       <h1>WEBSITE NEW</h1>
       <h2>test this part</h2>
-      <h2>test this part</h2>
            <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>

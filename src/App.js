0064407c18ec73151b8ad4caf829bc6e778ca37c
Saurabh <saurabhgola2006@gmail.com>
2024-11-14 17:58:30 +0530

    test branch-production

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ function App() {
   <>
     <div className="App">
      <h3>Netlify Production</h3>
-     <h1>Hello welcome WEBSITE</h1>
+     <h1>WEBSITE NEW</h1>
      <h3>netlify test new build</h3>
-     <h3>netlify test new build-test</h3>
       <br/>
-      <h1>WEBSITE NEW</h1>
-           <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>
     </div>

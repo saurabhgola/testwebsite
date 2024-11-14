9bbec527914d1a9fa54605e64e5b3ba9ad9efa5c
Saurabh <saurabhgola2006@gmail.com>
2024-11-14 17:27:17 +0530

    fix text-net-build changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
      <h3>Netlify Production</h3>
      <h1>Hello welcome WEBSITE</h1>
      <h3>netlify test new build</h3>
+     <h3>netlify test new build-test</h3>
       <br/>
       <h1>WEBSITE NEW</h1>
            <br/>

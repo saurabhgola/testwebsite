79fe48e4afc42bbe6ad02cfbcde585a3bbf44f9c
Saurabh <saurabhgola2006@gmail.com>
2024-11-14 16:18:10 +0530

    Fix leatest site-changes-two

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
      <h3>Netlify Production</h3>
      <h1>Hello welcome WEBSITE</h1>
      <h3>netlify test new build</h3>
-     <h1>new changes silver</h1>
+     <h1>new changes silver-two</h1>
       <br/>
       <h1>WEBSITE NEW</h1>
       <h2>test this part</h2>

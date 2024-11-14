8ce7822de05a1b46ccd06dc22cf43c78169c7ec0
saurabhgola <152872918+saurabhgola@users.noreply.github.com>
2024-11-14 18:29:36 +0530

    Create two (#40)
    * test new
    
    * new test
    
    * test new branch test
    
    * new read cd branch
    
    * test breanch develope
    
    * test branch2
    
    * test ready branch
    
    * test 5
    
    * test this main
    
    * new changes test
    
    * test  app json file
    
    * new changes djson
    
    * newd test this
    
    * newd test
    
    * new yaml file create
    
    * new app file1
    
    * new app file2
    
    * new branch
    
    * new branch Newdeveloped-new
    
    * new branch git
    
    * new branch git
    
    * new branch netlify
    
    * new branch netlify west
    
    * new branch netlify west yaml
    
    * new branch netlify west yaml
    
    * new netlify test
    
    * new netlify test-test
    
    * new netlify next
    
    * new netlify developed
    
    * new test-netlify
    
    * new test-site
    
    * new new-website
    
    * new website
    
    * Fix test
    
    * Fix test-new
    
    * Fix Netli test
    
    * Fix leatest site create
    
    * Fix leatest site create-that
    
    * Fix leatest site-dest
    
    * Fix leatest branch-new-t
    
    * Fix leatest branch-new-text
    
    * Fix leatest site-y
    
    * Fix leatest site-changes
    
    * Fix leatest site-changes-two
    
    * Fix leatest-new
    
    * fix text-new
    
    * fix text-net-build changes
    
    * test branch-production

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ function App() {
   return (
   <>
     <div className="App">
-      <h1>NEW-WEBSITE</h1>
      <h3>Netlify Production</h3>
-     <h1>Hello welcome WEBSITE</h1>
+     <h1>WEBSITE NEW</h1>
      <h3>netlify test new build</h3>
-     <h1>new changes silver-two</h1>
       <br/>
-      <h1>WEBSITE NEW</h1>
-      <h2>test this part</h2>
-           <br/>
       <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
  <br/>
     </div>

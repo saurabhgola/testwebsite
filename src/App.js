f32e9068b56d3a0416291428cd031f595acfb59d
Saurabh <saurabhgola2006@gmail.com>
2024-11-15 18:38:23 +0530

    tests branch yaml

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 
 function App() {
   return (
-  <>
     <div className="App">
-     <h3>Netlify Production</h3>
-     <h1>Hello welcome WEBSITE</h1>
-     <h3>netlify test new build</h3>
-     <h3>netlify create-deployed-test</h3>
-      <br/>
-      <h1>WEBSITE NEW</h1>
-           <br/>
-      <p className="App-intro">Current Enviornment: {process.env.REACT_APP_ENVIORNMENT}</p>
- <br/>
+      <div className="container">
+        <div className="content">
+          <div className="header">
+            <h3>Netlify Production</h3>
+            <h1>Hello, welcome to the WEBSITE</h1>
+            <h3>Netlify test new build</h3>
+            <h3>Netlify create-deployed-test</h3>
+            <br />
+            <h1>WEBSITE NEW</h1>
+            <br />
+            <p className="App-intro">
+              Current Environment: {process.env.REACT_APP_ENVIORNMENT}
+            </p>
+            <br />
+          </div>
+        </div>
+      </div>
     </div>
-  </>
   );
 }
 

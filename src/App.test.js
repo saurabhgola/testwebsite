5bf2d0343f3d4ffbde7a8c79220458aaf9b4b331
Saurabh <saurabhgola2006@gmail.com>
2024-11-19 10:58:12 +0530

    changes this-part

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  const element = screen.getByText(/some text/i);  // This is where the error happens
+  expect(element).toBeInTheDocument();
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
